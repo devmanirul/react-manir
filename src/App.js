426ab103ab3ce2144219f916313a3cecfb9850f1
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-07-01 21:01:43 +0600

    project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Maniral Islam</h1>
+      <p>mmimonir@gmail.com</p>
+      <button>monir</button>
+    </div>
+  );
+}
+
+export default App;
