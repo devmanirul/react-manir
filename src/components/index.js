13d3592cecfb9f6aa50ae32977e6a9cecfaba70c
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-07-26 21:02:29 +0600

    updates

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+import Home from "./Home";
+import Header from "./Header";
+
+const root = document.getElementById("root");
+ReactDOM.render(<Header/>, root);
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom";
-import Home from "./components/Home";
-import Header from "./components/Header";
-
-const root = document.getElementById("root");
-
-ReactDOM.render(<Header />, root);
